288095fd338a5d9cf7af889d79e531c1d1501951
Maru-rc <ruedamanuel007@gmail.com>
2026-03-11 10:01:27 -0500

    Refactor hero selection process in Heroes.py

--- Heroes.py
+++ Heroes.py
@@ -13,32 +13,15 @@ cuantos_dc = 0
 cuantos_marvel = 0
 
 while Salir != "no":
+ for i ,c in enumerate(heroes,start=1):
+        print(f"Heroe numero: {i}. Nombre = {c["nombre"]} | universo = {c["universo"]} | poder = {c["poder"]} | nivel = {c["nivel"]} ")
 
-    print(f""" 
----------------------------------------------------
-              Heroes Disponibles
----------------------------------------------------
-1. Nombre = {heroes[0]["nombre"]} | universo = {heroes[0]["universo"]} | poder = {heroes[0]["poder"]} | nivel = {heroes[0]["nivel"]}
-2. Nombre = {heroes[1]["nombre"]} | universo = {heroes[1]["universo"]} | poder = {heroes[1]["poder"]} | nivel = {heroes[1]["nivel"]}
-3. Nombre = {heroes[2]["nombre"]} | universo = {heroes[2]["universo"]} | poder = {heroes[2]["poder"]} | nivel = {heroes[2]["nivel"]}
-4. Nombre = {heroes[3]["nombre"]} | universo = {heroes[3]["universo"]} | poder = {heroes[3]["poder"]} | nivel = {heroes[3]["nivel"]}
-""")
-    
-    heroe_seleccionado = input("Que heroe vas a seleccionar?: ")
-
-    if heroe_seleccionado == "1":
-        equipo.append(heroes[0])
-
-    elif heroe_seleccionado == "2":
-        equipo.append(heroes[1])
-
-    elif heroe_seleccionado == "3":
-        equipo.append(heroes[2])
-
-    elif heroe_seleccionado == "4":
-        equipo.append(heroes[3])
-    
-    Salir = input("Quieres seguir agregando heroes al equipo? si/no: ")
+ heroe_seleccionado = int(input("\nQue heroe vas a seleccionar?: "))
+ if heroes[heroe_seleccionado-1] in equipo:
+       print("\nYa este heroe esta en tu equipo, selecciona otro:")
+ else:
+        equipo.append(heroes[heroe_seleccionado-1])
+        Salir = input("\nQuieres seguir agregando heroes al equipo? si/no: ")
 
 print(f""" 
 ---------------------------------------------------
